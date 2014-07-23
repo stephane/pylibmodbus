561f87080bc4fcbd760c058ed2036927c86bd36c
ofaletse <ofaletse@ska.ac.za>
2014-07-23 11:36:17 +0200

    Always return a value

--- pylibmodbus/modbus_core.py
+++ pylibmodbus/modbus_core.py
@@ -50,6 +50,7 @@ class ModbusCore(object):
         rc = func(self.ctx, *args)
         if rc == -1:
             raise Exception(ffi.string(C.modbus_strerror(ffi.errno)))
+        return rc
 
     def connect(self):
         return self._run(C.modbus_connect)
