fc34139f056d78f6f8fee9b774a1e6a85725e07d
michaelDovgal <dovgalmichael@gmail.com>
2016-04-08 17:01:58 +0300

    fixing creating ModbusRtu instance error

--- pylibmodbus/modbus_rtu.py
+++ pylibmodbus/modbus_rtu.py
@@ -8,4 +8,4 @@ from .modbus_core import C, ModbusCore
 
 class ModbusRtu(ModbusCore):
     def __init__(self, device="/dev/ttyS0", baud=19200, parity="N", data_bit=8, stop_bit=1):
-        self.ctx = C.modbus_new_rtu(device, baud, parity, data_bit, stop_bit)
+        self.ctx = C.modbus_new_rtu(device.encode(), baud, parity.encode(), data_bit, stop_bit)
