8136877b743e8d49eb91aa9a88ea6158207267a4
Ray <zsirui@126.com>
2023-12-04 11:54:07 +0800

    Add modbus_tcp_listen and modbus_tcp_accept function

--- pylibmodbus/modbus_tcp.py
+++ pylibmodbus/modbus_tcp.py
@@ -8,3 +8,9 @@ from .modbus_core import C, ModbusCore
 class ModbusTcp(ModbusCore):
     def __init__(self, ip="127.0.0.1", port=502):
         self.ctx = C.modbus_new_tcp(ip.encode(), port)
+
+	def modbus_tcp_listen(self, nb_connection):
+		self._run(libmodbus.modbus_tcp_listen, nb_connection)
+
+	def modbus_tcp_accept(self, s):
+		self._run(libmodbus.modbus_tcp_listen, s)
